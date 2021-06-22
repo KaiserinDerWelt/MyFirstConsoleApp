2d95e52e8a40d695b65e9be761ee252ca48cc359
Mariana Licona <m.licona@abtis.de>
2021-06-22 02:29:06 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OperatorExamples();  //Here I am calling the Operator examples method that is inside of  the main class.
+        }
+
+        private static void OperatorExamples()
+        {
+            int width = 3;
+            width++;
+
+            int height = 2 + 4;
+            int area = width * height;
+            Console.WriteLine(area);
+
+            string result = "The area";
+            result = result + " is " + area;
+            Console.WriteLine(result);
+
+            bool truthValue = true;
+            Console.WriteLine(truthValue);
+        }
+    }
+}
