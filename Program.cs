077a94b065fdb94b949aab7d8aa62ed2cb412e1c
Mariana Licona <m.licona@abtis.de>
2021-06-22 18:52:03 +0200

    learn while, do  and for loops

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ namespace MyFirstConsoleApp
         static void Main(string[] args)
         {
             OperatorExamples();  //Here I am calling the Operator examples method that is inside of  the main class.
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine("ITERATION" + i);
+                 }
+        
         }
 
         private static void OperatorExamples()
@@ -17,6 +22,15 @@ namespace MyFirstConsoleApp
             int height = 2 + 4;
             int area = width * height;
             Console.WriteLine(area);
+            while (area <20)
+            {
+                height++;
+                area = width * height;
+            }
+            do
+            {
+
+            } while (area>25);
 
             string result = "The area";
             result = result + " is " + area;
